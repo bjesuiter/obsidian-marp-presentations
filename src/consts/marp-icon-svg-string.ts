ad528ef76d37b3cb8be8af406421af99c973ef30
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 17:05:31 +0200

    adjust size of marp icon

--- src/consts/marp-icon-svg-string.ts
+++ src/consts/marp-icon-svg-string.ts
@@ -1,5 +1,5 @@
 export const marpIconSvgString = `
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" class="block h-16 w-16 p-2 md:p-3 md:h-20 md:w-20">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="18" height="18">
 <path fill="#009bda" d="M40 0L0 40h20l20-20V0z"></path>
 <path fill="#78c5e9" d="M40 20v20h20V0L40 20z"></path>
 <path fill="#007aad" d="M20 40h20V20L20 40z">
