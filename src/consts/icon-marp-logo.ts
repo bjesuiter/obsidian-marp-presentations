beb94928a4960f4254c467f8ebb6dfecca119e91
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 17:16:23 +0200

    rename file with marp logo icon to better fit generral naming scheme

--- src/consts/icon-marp-logo.ts
+++ src/consts/icon-marp-logo.ts
@@ -1,4 +1,7 @@
-export const marpIconSvgString = `
+/**
+ * Can be attachd to a node via element.innerHtml = iconMarpLogo
+ */
+export const iconMarpLogo = `
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 40" width="18" height="18">
 <path fill="#009bda" d="M40 0L0 40h20l20-20V0z"></path>
 <path fill="#78c5e9" d="M40 20v20h20V0L40 20z"></path>
--- src/features /marp-presentation-view/generate-obsidian-view-header.ts
+++ src/features /marp-presentation-view/generate-obsidian-view-header.ts
@@ -1,9 +1,9 @@
-import { marpIconSvgString } from '../../consts/marp-icon-svg-string';
+import { iconMarpLogo } from '../../consts/icon-marp-logo';
 export function generateObsidianViewHeader(containerEl: HTMLElement) {
 	const header = containerEl.createDiv({ cls: 'view-header' });
 
 	const icon = header.createDiv({ cls: 'view-header-icon' });
-	icon.innerHTML = marpIconSvgString;
+	icon.innerHTML = iconMarpLogo;
 
 	const titleContainer = header.createDiv({ cls: 'view-header-title-container' });
 	const title = titleContainer.createDiv({ cls: 'view-header-title' });
