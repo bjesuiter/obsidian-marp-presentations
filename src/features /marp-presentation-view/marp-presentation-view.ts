8bd81f03bd8166d2d1a4b3cf2ff69c9eeabda238
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 16:56:33 +0200

    add basic obsidian ui around my plugin content

--- src/features /marp-presentation-view/marp-presentation-view.ts
+++ src/features /marp-presentation-view/marp-presentation-view.ts
@@ -1,6 +1,33 @@
-import { View } from 'obsidian';
+import { View, WorkspaceLeaf } from 'obsidian';
 
 export class MarpPresentationView extends View {
+	leaf: WorkspaceLeaf;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+		this.leaf = leaf;
+	}
+
+	async onOpen() {
+		this.display();
+	}
+
+	display() {
+		const { containerEl } = this;
+
+		const header = containerEl.createDiv({ cls: 'view-header' });
+
+		const icon = header.createDiv({ cls: 'view-header-icon' });
+		const titleContainer = header.createDiv({ cls: 'view-header-title-container' });
+		const title = titleContainer.createDiv({ cls: 'view-header-title' });
+		title.textContent = 'MARP Presentation View';
+		const actions = header.createDiv({ cls: 'view-actions' });
+
+		const body = containerEl.createDiv({ cls: 'view-content' });
+
+		body.createEl('h2', { text: 'My MARP Presentation View' });
+	}
+
 	getViewType(): string {
 		return 'marp-presentation';
 	}
