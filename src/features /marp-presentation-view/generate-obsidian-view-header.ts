36c1664c070a92876c362087fc71cedf159441a1
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 17:21:19 +0200

    add working marp workspaceLeaf close

--- src/features /marp-presentation-view/generate-obsidian-view-header.ts
+++ src/features /marp-presentation-view/generate-obsidian-view-header.ts
@@ -1,21 +1,29 @@
+import { WorkspaceLeaf } from 'obsidian';
 import { iconObsidianClose } from 'src/consts/icon-obsidian-close';
 import { iconMarpLogo } from '../../consts/icon-marp-logo';
 
-export function generateObsidianViewHeader(containerEl: HTMLElement) {
+export function generateObsidianViewHeader(containerEl: HTMLElement, leaf: WorkspaceLeaf) {
 	const header = containerEl.createDiv({ cls: 'view-header' });
 
+	// generates header icon
 	const icon = header.createDiv({ cls: 'view-header-icon' });
 	icon.innerHTML = iconMarpLogo;
 
+	// generates header title
 	const titleContainer = header.createDiv({ cls: 'view-header-title-container' });
 	const title = titleContainer.createDiv({ cls: 'view-header-title' });
 	title.textContent = 'MARP Presentation View';
 
 	const actions = header.createDiv({ cls: 'view-actions' });
+
+	// generates close action button
 	const closeAction = actions.createEl('a', {
 		cls: 'view-action mod-close-leaf',
 	});
 	closeAction.innerHTML = iconObsidianClose;
+	closeAction.onClickEvent(() => {
+		leaf.detach();
+	});
 
 	return {
 		header,
