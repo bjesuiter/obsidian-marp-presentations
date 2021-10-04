a6d4d6e4507800a9d261e1cefd95aa70b518ddf6
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 14:36:10 +0200

    add MarpPresentationView Class

--- src/features /marp-presentation-view/marp-presentation-view-factory.ts
+++ src/features /marp-presentation-view/marp-presentation-view-factory.ts
@@ -1,3 +1,6 @@
 import { View, WorkspaceLeaf } from 'obsidian';
+import { MarpPresentationView } from './marp-presentation-view';
 
-export function marpPresentationViewFactory(leaf: WorkspaceLeaf): View {}
+export function marpPresentationViewFactory(leaf: WorkspaceLeaf): View {
+	return new MarpPresentationView(leaf);
+}
---
+++ src/features /marp-presentation-view/marp-presentation-view.ts
@@ -0,0 +1,11 @@
+import { View } from 'obsidian';
+
+export class MarpPresentationView extends View {
+	getViewType(): string {
+		throw new Error('Method not implemented.');
+	}
+
+	getDisplayText(): string {
+		throw new Error('Method not implemented.');
+	}
+}
