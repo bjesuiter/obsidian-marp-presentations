af869275ed88aca434e51055b35e8d8669c80067
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 17:38:40 +0200

    can now detect markdown view in state 'source'

--- src/marp-plugin.ts
+++ src/marp-plugin.ts
@@ -4,6 +4,8 @@ import { MarpSettingsTab } from './features /marp-settings-tab/marp-settings-tab
 import { MarpPluginSettings } from './types/marp-plugin-settings';
 import { marpPresentationViewFactory } from './features /marp-presentation-view/marp-presentation-view-factory';
 import { logger } from './consts/logger';
+import { MarkdownViewState } from './types/markdown-view-state';
+import { stat } from 'fs';
 
 export class MarpPlugin extends Plugin {
 	settings: MarpPluginSettings;
@@ -26,15 +28,20 @@ export class MarpPlugin extends Plugin {
 		this.addCommand({
 			id: 'marp-open-presentation-view',
 			name: 'Open Presentation View',
-			callback: () => {
-				logger.log('Opening presentation view ...');
-				// the last param shows the new split pane on the left side of the current leaf
-				const newWorkspaceLeaf = this.app.workspace.createLeafBySplit(
-					this.app.workspace.getLeaf(),
-					'vertical',
-					false
-				);
-				newWorkspaceLeaf.open(marpPresentationViewFactory(newWorkspaceLeaf));
+			checkCallback: (obsidianIsChecking) => {
+				if (!obsidianIsChecking) {
+					this.openPresentationView();
+					return;
+				}
+
+				const { type, state } = this.app.workspace.activeLeaf.getViewState();
+				logger.log(`activeView state: `, state);
+				if (type === 'markdown' && (state as MarkdownViewState).mode === 'source') {
+					logger.log('Active leaf is markdown source view!');
+					return true;
+				}
+
+				return false;
 			},
 		});
 
@@ -66,6 +73,17 @@ export class MarpPlugin extends Plugin {
 
 		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 		logger.log('loading finshed');
+	}
+
+	openPresentationView() {
+		logger.log('Opening presentation view ...');
+		// the last param shows the new split pane on the left side of the current leaf
+		const newWorkspaceLeaf = this.app.workspace.createLeafBySplit(
+			this.app.workspace.getLeaf(),
+			'vertical',
+			false
+		);
+		newWorkspaceLeaf.open(marpPresentationViewFactory(newWorkspaceLeaf));
 	}
 
 	onunload() {
