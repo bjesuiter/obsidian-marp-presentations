c0666e36692c9c97d401e0f5ba7ffdbea2f403ea
Benjamin Jesuiter <bjesuiter@gmail.com>
2021-10-04 15:10:50 +0200

    emove unused imports

--- main.ts
+++ main.ts
@@ -1,9 +1,3 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { MarpPluginSettings } from './src/types/marp-plugin-settings';
 import { MarpPlugin } from './src/marp-plugin';
 
 export default MarpPlugin;
-
-
-
-
