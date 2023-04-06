7daa0813214d1b042e93dfbb340e6a8eb914e414
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-04-06 13:21:02 +0200

    fix rollup config output path

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ if you want to view the source visit the plugins github repository
 
 // const outDir = `dist`
 // Hack: To generate all output directly into an obsidian vault for testing:
-const outDir = `../plugin-playground/.obsidian/plugins/obsidian-marp-presentations`;
+const outDir = `./test-marp-plugin/.obsidian/plugins/obsidian-marp-presentations`;
 
 export default {
 	input: 'main.ts',
